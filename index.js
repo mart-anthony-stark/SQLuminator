4a23c0bd505c7bf93ace9ae30a48f86cc924b860
mart-anthony-stark <martsalazar0@gmail.com>
2023-06-16 01:00:35 +0800

    change build script

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const { Model } = require("./dist/lib/es5");
-const { SQL } = require("./dist/lib/es5/core/sql");
+const { SQL } = require("./dist/lib/es5");
 
+SQL.createConnection("mysql", {
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "orm",
+  port: 3306,
+});
 const User = new Model("user");
 const server = async () => {
   // const newUser = await User.save({ name: "mart" });
   // console.log({ newUser });
-  // const users = await User.find();
-  // console.log(users);
+  const users = await User.find({ lean: true });
+  console.log(users);
 };
 server();
